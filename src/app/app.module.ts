00adee1566647545e238edb434603e55fe205dcb
Andre Tranchezzi <andretranchezzi@MacBook-Pro-de-Andre.local>
2020-08-15 20:50:40 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+
+import {InputMaskModule} from 'primeng/inputmask';
+import {KeyFilterModule} from 'primeng/keyfilter';
+import {FullCalendarModule} from 'primeng/fullcalendar';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    InputMaskModule,
+    KeyFilterModule,
+    ButtonModule,
+    FullCalendarModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
